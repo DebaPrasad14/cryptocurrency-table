af227f64a45b906093115e244bc47d8c56c292ad
debaprasad14 <debaprasad@eichiba.com>
2020-11-25 00:18:58 +0530

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <div id="app">
+    <h1>Cryptocurrency App</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "app",
+};
+</script>
+
+<style>
+body {
+  background: #fefefe;
+  margin: auto;
+  padding: auto;
+}
+</style>
